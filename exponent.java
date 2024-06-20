ebe5ed0c35200320d00cf10ecbb1b62c7253dad1
Swathi-Reddymalla <swathireddymalla08@gmail.com>
2024-06-20 14:53:24 +0530

    Create exponent.java

---
+++ exponent.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Exponent
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int m,n;
+double result = Math.pow(2, 3);
+System.out.println(result);
+}
+}
+
+
+
+OUTPUT
+8.0
